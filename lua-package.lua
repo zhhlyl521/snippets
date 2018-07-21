445b8899b82082962635604794dd1d80bbecd080
zhh <34538993+zhhlyl521@users.noreply.github.com>
2018-07-21 16:36:05 +0800

    Update lua-package.lua

--- lua-package.lua
+++ lua-package.lua
@@ -1,9 +1,7 @@
 -- producer
 local userinfo_get = {}
 function userinfo_get.foo(userID)
-
   -- Operation to obtain user information
-
   return userinfo
 end
 return userinfo_get
@@ -12,14 +10,11 @@ return userinfo_get
 local new = require "userinfo_get"
 local cjson = require "cjson"
 local userinfo=new.foo(1)
-
 ngx.log(ngx.INFO,cjson.encode(userinfo))
-
 local result = cjson.encode({resultCode = "1001"})
 ngx.log(ngx.INFO,"<result>" .. result)
 ngx.say(result)
 return ngx.exit(ngx.HTTP_OK)
-
 
 
 -- After testing,this approach can deal with concurrency problems.
